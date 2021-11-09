5a3ed21d34108b42c14f3cc3e64c86f3c7a0fad1
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2021-11-09 18:50:46 -0500

    README added commits

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Note Taker Starter Code
+
+## Credits 
+Rutger coding Bootcamp starter code edited by Edgar.Q 
+
+## Install 
+Npm install 
+
+npm start 
+
+## Testing 
+This app was intended to take notes and store them. 
+
+## License 
+Copyright Edgar,Q 2021 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "software"), to deal in the software with out restriction, including without limitation the rights to use, copy, modify, merge, publish. The above copyright notice and this permisssion notice shall be inluded in all copies or substantial portions of the Software.
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+//paths
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -22,11 +23,12 @@ app.post('/api/notes', (req, res) => {
   });
   }); 
 });
+
 /*  
 app.delete('/api/notes/:id', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
-    let notes = JSON.parse(data);
+    let notes = JSON.parse(data); 
     const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
   
   fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
@@ -40,7 +42,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });   
 
-
+//Api 
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
@@ -60,3 +62,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
 });
+// End of api 
